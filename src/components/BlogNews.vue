48aa5e2f662442d0d4c06122383ce8469b05f1c6
Filipe <filipe.matias@globalbot.com>
2022-03-21 21:22:33 -0300

    Daily

--- src/components/BlogNews.vue
+++ src/components/BlogNews.vue
@@ -10,11 +10,11 @@
             by: Filipe 20/03/2022
           </div>
           <div class="text-body1 q-pt-md" >
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged..
+            Lorem Ipsum is simply dummy text of the printing andk a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged..
           </div>
         </q-card-section>
         <q-card-section class="col-3">
-          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" style="max-height: 165px"/>
           <q-card-actions align="right">
             <q-btn color="primary" icon-right="arrow_right_alt" label="Ler mais" flat />
           </q-card-actions>
@@ -27,10 +27,18 @@
 <script>
 export default {
   name: "BlogNews",
-  props:{
-    imgSrc:{
+  props: {
+    articleImg: {
       type: String,
       required: true
+    },
+    articleTitle: {
+      type: String,
+      required: true
+    },
+    articleContent: {
+      type: String,
+      require: true
     }
   }
 }
--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="text-h3 q-pa-md" style="color: #1976d2">Artigos</div>
-  <BlogNews />
+  <BlogNews
+    article-img="https://cdn.quasar.dev/img/parallax2.jpg"
+    article-content="Lorem Ipsum is simply dummy text of the printing andk a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.."
+    article-title="Titulo do artigo, sobre o javascript framework quasar"
+  />
   <BlogNews />
   <BlogNews />
 </template>
